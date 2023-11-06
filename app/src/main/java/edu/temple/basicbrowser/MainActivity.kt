48d9b61d30414da905d56683f6a404234b8fe741
Alex09190 <tuj09190@temple.edu>
2023-11-06 15:46:04 -0500

    commit1

--- app/src/main/java/edu/temple/basicbrowser/MainActivity.kt
+++ app/src/main/java/edu/temple/basicbrowser/MainActivity.kt
@@ -21,10 +21,31 @@ class MainActivity : AppCompatActivity() {
         goButton = findViewById(R.id.goButton)
         webView = findViewById(R.id.webView)
 
+        var tempString : String? = null
+        var tempInt : Int = 0
+
+        //myButton.setOnClickListener { doSomething((it as Button).text) }
+
+        goButton.setOnClickListener() {
+            tempString = "www.YouTube.com"//urlEditText.text.toString()
+            tempInt++
+        }
+
+
+        //val volleyQueue = Volley.newRequestQueue(this)
+        //goButton.callOnClick()
+
         // Allow your browser to intercept hyperlink clicks
         webView.webViewClient = object: WebViewClient() {
             override fun onPageFinished(view: WebView?, url: String?) {
                 super.onPageFinished(view, url)
+
+
+
+                    //tempString = "www.YouTube.com"
+                    tempString?.let { webView.loadUrl(it) }
+
+
             }
         }
 
